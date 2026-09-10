cb9e686399aa84637dc51b36daa8c3dce9da7952
anke180 <anke.v.dommelen@gmail.com>
2026-09-10 14:34:56 +0200

    Exorcise testing

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@ namespace ObserverPattern
         {
             WeatherData weatherData = new WeatherData();
 
-            // Create instances of displays 
+            var currentDisplay = new CurrentConditionDisplay(weatherData);
+            var forecastDisplay = new ForecastDisplay(weatherData);
+            var statisticsDisplay = new StatisticsDisplay(weatherData);
 
             weatherData.SetMeasurements(28, 65, 30.4f);
             weatherData.SetMeasurements(29, 70, 29.2f);
